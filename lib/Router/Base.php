5d04c2f790fe9b3d62e90a91e6e4697f8991dd02
Mike Kruk <tamagokun@gmail.com>
2012-02-12 18:13:01 -0500

    added Base.php (it's rackable!)

---
+++ lib/Router/Base.php
@@ -0,0 +1,51 @@
+<?php
+namespace Router;
+
+class Base
+{
+	protected $app;
+	protected $conditions;
+	protected $routes;
+	protected $filters;
+	protected $errors;
+	
+	public function __construct($app)
+	{
+		$this->app = $app;
+	}
+	
+	public function call($env)
+	{
+		$this->env = $env;
+		$this->request = new \Rackem\Request($env);
+		$this->response = new \Rackem\Response();
+		$this->params = $this->request->params();
+		
+		$this->reponse->finish();
+	}
+	
+	//private
+	private function dispatch()
+	{
+		try
+		{
+			//if( settings->static )
+			$this->filter("before");
+			$this->route();
+		}catch(Exception $e)
+		{
+			
+		}
+		$this->filter("after");
+	}
+	
+	private function filter($where)
+	{
+		
+	}
+	
+	private function route()
+	{
+			
+	}
+}
