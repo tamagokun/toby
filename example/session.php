308e3c1708833a21551dee5f2f52f63f16037c17
Mike Kruk <mike.kruk@mslgroup.com>
2012-03-15 09:44:27 -0400

    session setting appears to be working. Need to try some cookies.

--- example/session.php
+++ example/session.php
@@ -3,11 +3,13 @@
 require 'rackem/rackem.php';
 require dirname(__FILE__).'/../router.php';
 
-$app = new \Rackem\Base();
+$app = new \Router\Base();
 
 $app->enable("sessions");
 
 $app->get("/", function($app) {
-	$app->session("value") = "Hello World!";
-	return "The cookie you created contains the value: {$app->session('value')}";
+	$app->env["rack.session"]["value"] = "Hello World!";
+	return "The cookie you created contains the value: {$app->request->session('value')}";
 });
+
+$app->run();
