0f7c2cfac530ac39b3c0aca4c115e850c9fc1fbe
Mike Kruk <tamagokun@gmail.com>
2013-03-14 08:48:27 -0400

    use the github API to grab the docs.

--- js/toby.js
+++ js/toby.js
@@ -1,6 +1,8 @@
 var analytics=analytics||[];analytics.load=function(e){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src=("https:"===document.location.protocol?"https://":"http://")+"d2dq2ahtl5zl1z.cloudfront.net/analytics.js/v1/"+e+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);var r=function(e){return function(){analytics.push([e].concat(Array.prototype.slice.call(arguments,0)))}},i=["identify","track","trackLink","trackForm","trackClick","trackSubmit","pageview","ab","alias","ready"];for(var s=0;s<i.length;s++)analytics[i[s]]=r(i[s])};
 analytics.load("5g9bdq58dz");
 
+var sha = 'f7d530c199e7a017cb521b5356e41c42db598fce';
+
 jQuery(document).ready(function($) {
 	//set up marked
 	marked.setOptions({
@@ -21,9 +23,14 @@ jQuery(document).ready(function($) {
 	};
 	$(window).on('scroll', spy_nav);
 
-	$.ajax('https://raw.github.com/tamagokun/toby/master/docs/docs.md', {
-		success: function(markdown) {
-			var lexed = marked.lexer(markdown);
+	$.ajax('https://api.github.com/repos/tamagokun/toby/git/blobs/'+sha, {
+		dataType: "jsonp",
+		success: function(data) {
+			if(typeof data.data.content == "undefined") return;
+			var content = data.data.content;
+			if(data.data.encoding == "base64")
+				content = window.atob(content.replace(/\n/g,""));
+			var lexed = marked.lexer(content);
 			var parsed = marked.parser(lexed);
 			//TODO: Loader?
 			$("#main").append(parsed);
