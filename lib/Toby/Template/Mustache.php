741fc40d09a9a1c5e36d9aabdc5bd395ff2f1b87
Mike Kruk <tamagokun@gmail.com>
2013-08-23 16:35:11 -0400

    Mustache template should use Mustache_Engine

--- lib/Toby/Template/Mustache.php
+++ lib/Toby/Template/Mustache.php
@@ -5,9 +5,9 @@ class Mustache extends \Toby\Template
 {
 	public function render($locals,$yield)
 	{
-		if(!class_exists("\\Mustache")) throw new \Rackem\Exception("Required class 'Mustache' not found.");
+		if(!class_exists("\\Mustache_Engine")) throw new \Rackem\Exception("Required class 'Mustache_Engine' not found.");
 		$template = file_get_contents($this->file);
-		$engine = new \Mustache();
+		$engine = new \Mustache_Engine();
 		$locals["yield"] = $yield;
 		ob_start();
 		echo $engine->render($template,$locals);
